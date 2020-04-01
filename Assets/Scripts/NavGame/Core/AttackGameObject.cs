49001d3480eac4aeed6ef3220f626c56d6a40f6b
JuanDaviAlves <juanhasbene@hotmail.com>
2020-04-01 01:31:43 -0300

    Movendinho o NMA para Atacar o JogoObjeto

--- Assets/Scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NavGame.Managers;
+using UnityEngine.AI;
 
 namespace NavGame.Core
 {
+    [RequireComponent(typeof(NavMeshAgent))]
     public class AttackGameObject : TouchableGameObject
     {
         public OfenseStats ofenseStats;
-
+        protected NavMeshAgent agent;
         float cooldown = 0f;
-
         public OnAttackHitEvent onAttackHit;
 
+        protected virtual void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
         protected virtual void Update()
         {
             DecreaseAttackCooldown();
         }
         public void AttackOnCooldown(DamageableGameObject target)
         {
-            if(cooldown <= 0f)
+            if (cooldown <= 0f)
             {
-                cooldown = 1f/ofenseStats.attackSpeed;
+                cooldown = 1f / ofenseStats.attackSpeed;
                 target.TakeDamage(ofenseStats.damage);
-                if(onAttackHit!= null)
+                if (onAttackHit != null)
                 {
                     onAttackHit(target.transform.position);
                 }
@@ -32,12 +37,12 @@ namespace NavGame.Core
 
         void DecreaseAttackCooldown()
         {
-            if(cooldown == 0f)
+            if (cooldown == 0f)
             {
                 return;
             }
             cooldown -= Time.deltaTime;
-            if(cooldown < 0f)
+            if (cooldown < 0f)
             {
                 cooldown = 0f;
             }
