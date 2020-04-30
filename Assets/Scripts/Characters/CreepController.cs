ba44e52fcb348af08dcc4df3d38a099c2631093f
JuanDaviAlves <juanhasbene@hotmail.com>
2020-04-30 20:02:57 -0300

    Sons de Danoninhos

--- Assets/Scripts/Characters/CreepController.cs
+++ Assets/Scripts/Characters/CreepController.cs
@@ -19,7 +19,6 @@ public class CreepController : AttackGameObject
         {
             finalTarget = obj.GetComponent<DamageableGameObject>();
         }
-        onAttackHit += PlayEffects;
     }
     protected override void Update()
     {
@@ -43,10 +42,5 @@ public class CreepController : AttackGameObject
         {
             agent.SetDestination(finalTarget.transform.position);
         }
-    }
-
-    void PlayEffects(Vector3 position)
-    {
-        AudioManager.instance.Play("enemy-hit", position);
     }
 }
